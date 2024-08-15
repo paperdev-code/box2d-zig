3c9f7bf9aba5f5f3119c816137a399485db0578b
paperdev-code <51487819+paperdev-code@users.noreply.github.com>
2024-08-15 22:03:02 +0200

    note on @cImport

--- src/box2d.zig
+++ src/box2d.zig
@@ -1,4 +1,6 @@
 /// C API directly from Box2D.
+/// TODO: replace with TranslateCStep when it accepts lazypaths.
+/// https://github.com/ziglang/zig/issues/20630
 pub const c = @cImport({
     @cInclude("box2d/box2d.h");
 });
